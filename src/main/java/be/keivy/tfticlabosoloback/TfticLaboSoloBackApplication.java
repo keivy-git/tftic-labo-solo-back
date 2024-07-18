0e4aa24301423798023adf4329c531ce227bdf7a
Kei-gits <kioo-17@hotmail.com>
2024-07-18 06:59:49 +0200

    add init project

---
+++ src/main/java/be/keivy/tfticlabosoloback/TfticLaboSoloBackApplication.java
@@ -0,0 +1,13 @@
+package be.keivy.tfticlabosoloback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfticLaboSoloBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfticLaboSoloBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/keivy/tfticlabosoloback/TfticLaboSoloBackApplicationTests.java
@@ -0,0 +1,13 @@
+package be.keivy.tfticlabosoloback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfticLaboSoloBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
