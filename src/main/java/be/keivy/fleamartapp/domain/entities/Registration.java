494a59de2b5262f243b65ad214c11a91f80e1c09
Kei-gits <kioo-17@hotmail.com>
2024-07-30 10:30:16 +0200

    feat Registration

---
+++ src/main/java/be/keivy/fleamartapp/domain/entities/Registration.java
@@ -0,0 +1,28 @@
+package be.keivy.fleamartapp.domain.entities;
+
+import be.keivy.fleamartapp.domain.enums.SubsStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "registration")
+public class Registration extends BaseEntity<Long> {
+
+    @Column(name = "subs_date_end", nullable = false)
+    private LocalDateTime subsDateEnd;
+
+    @Column(name = "subs_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SubsStatus subsStatus;
+
+    @Column(name ="reserved_meter", nullable = false)
+    private int reservedMeter;
+}
