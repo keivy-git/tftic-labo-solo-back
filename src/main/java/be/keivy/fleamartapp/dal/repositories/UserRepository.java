8fc9ff679f2ed70cf798491f0a87cd16aee9c97d
Kei-gits <kioo-17@hotmail.com>
2024-07-30 10:32:05 +0200

    feat user e

---
+++ src/main/java/be/keivy/fleamartapp/dal/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package be.keivy.fleamartapp.dal.repositories;
+
+import be.keivy.fleamartapp.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u where u.email = :email")
+    Optional<User> findByEmail(String email);
+
+    @Query("select u from User u where u.phoneNumber = :phoneNumber")
+    Optional<User> findByPhone(String phoneNumber);
+}
