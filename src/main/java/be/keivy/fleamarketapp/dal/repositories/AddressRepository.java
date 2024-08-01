3a3662476453098c0974b43b256ff9a5c6879eb4
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-08-01 16:11:26 +0200

    feat address

--- src/main/java/be/keivy/fleamarketapp/dal/repositories/AddressRepository.java
+++ src/main/java/be/keivy/fleamarketapp/dal/repositories/AddressRepository.java
@@ -1,6 +1,6 @@
-package be.keivy.fleamartapp.dal.repositories;
+package be.keivy.fleamarketapp.dal.repositories;
 
-import be.keivy.fleamartapp.domain.entities.Address;
+import be.keivy.fleamarketapp.domain.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/be/keivy/fleamarketapp/domain/entities/Address.java
+++ src/main/java/be/keivy/fleamarketapp/domain/entities/Address.java
@@ -1,4 +1,4 @@
-package be.keivy.fleamartapp.domain.entities;
+package be.keivy.fleamarketapp.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
