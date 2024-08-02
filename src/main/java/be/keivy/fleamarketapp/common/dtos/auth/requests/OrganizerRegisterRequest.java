bdbc1d10d8ac33d1baaeb84ccb734871ce695d12
Kei-gits <kioo-17@hotmail.com>
2024-08-02 23:38:49 +0200

    addd getrole Name

--- src/main/java/be/keivy/fleamarketapp/common/dtos/auth/requests/OrganizerRegisterRequest.java
+++ src/main/java/be/keivy/fleamarketapp/common/dtos/auth/requests/OrganizerRegisterRequest.java
@@ -1,9 +1,12 @@
 package be.keivy.fleamarketapp.common.dtos.auth.requests;
 
+import be.keivy.fleamarketapp.dal.repositories.RoleRepository;
 import be.keivy.fleamarketapp.domain.entities.Address;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import static org.hibernate.metamodel.mapping.internal.AnyDiscriminatorPart.ROLE_NAME;
 
 @Getter @Setter
 @AllArgsConstructor
@@ -21,5 +24,8 @@ public class OrganizerRegisterRequest extends RegisterRequest {
     @NotBlank(message = "There must be a address")
     private Address address;
 
-
+    @Override
+    public String getRoleName() {
+        return "";
+    }
 }
