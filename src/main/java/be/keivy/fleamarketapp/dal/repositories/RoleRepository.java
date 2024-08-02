9d2bb8a41c2917e82bcd7ad5e1ad907ea22d7705
Kei-gits <kioo-17@hotmail.com>
2024-08-02 23:39:14 +0200

    add RoleRepository findByName

--- src/main/java/be/keivy/fleamarketapp/dal/repositories/RoleRepository.java
+++ src/main/java/be/keivy/fleamarketapp/dal/repositories/RoleRepository.java
@@ -2,8 +2,14 @@ package be.keivy.fleamarketapp.dal.repositories;
 
 import be.keivy.fleamarketapp.domain.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    @Query("SELECT r FROM Role r WHERE r.name = :name")
+    Optional<Role> findByName(String name);
 }
