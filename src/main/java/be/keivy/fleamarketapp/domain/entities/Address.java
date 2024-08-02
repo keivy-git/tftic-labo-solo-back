425bfdefabe99652f8bfcf93aa7f0b64c1c7838d
Kei-gits <kioo-17@hotmail.com>
2024-08-02 23:36:57 +0200

    Add address

--- src/main/java/be/keivy/fleamarketapp/domain/entities/Address.java
+++ src/main/java/be/keivy/fleamarketapp/domain/entities/Address.java
@@ -9,6 +9,11 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Table(name="address")
+/**
+ * street
+ * city
+ * zip
+ */
 public class Address extends BaseEntity<Long> {
 
     @Column(name="street", nullable = false)
