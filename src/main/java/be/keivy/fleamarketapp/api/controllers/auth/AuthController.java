98018ab487ef8e0789412876992475bed7eab00e
Kei-gits <kioo-17@hotmail.com>
2024-08-04 22:13:57 +0200

    remove import

--- src/main/java/be/keivy/fleamarketapp/api/controllers/auth/AuthController.java
+++ src/main/java/be/keivy/fleamarketapp/api/controllers/auth/AuthController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
