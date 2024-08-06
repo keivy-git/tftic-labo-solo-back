9f8f835e2b0b2f8bf84b6f7d4e0004d7c71a38d7
Kei-gits <kioo-17@hotmail.com>
2024-08-06 16:30:51 +0200

    update path

--- src/main/java/be/keivy/fleamarketapp/common/mappers/flea_market/FleaMarketMapper.java
+++ src/main/java/be/keivy/fleamarketapp/common/mappers/flea_market/FleaMarketMapper.java
@@ -3,6 +3,7 @@ package be.keivy.fleamarketapp.common.mappers.flea_market;
 import be.keivy.fleamarketapp.common.dtos.flea_market.requests.FleaMarketRequest;
 import be.keivy.fleamarketapp.common.dtos.flea_market.responses.FleaMarketPagedResponse;
 import be.keivy.fleamarketapp.common.dtos.flea_market.responses.FleaMarketResponse;
+import be.keivy.fleamarketapp.domain.entities.Address;
 import be.keivy.fleamarketapp.domain.entities.FleaMarket;
 import be.keivy.fleamarketapp.domain.entities.Organizer;
 import org.mapstruct.Mapper;
@@ -16,10 +17,10 @@ public interface FleaMarketMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(source = "request.address.street", target = "address.street")
-    @Mapping(source = "request.address.zipCity.city", target = "address.zipCity.city")
-    @Mapping(source = "request.address.zipCity.zip", target = "address.zipCity.zip")
-    FleaMarket toEntity(FleaMarketRequest request, Organizer organizer);
+    @Mapping(source = "address.street", target = "address.street")
+    @Mapping(source = "address.zipCity.city", target = "address.zipCity.city")
+    @Mapping(source = "address.zipCity.zip", target = "address.zipCity.zip")
+    FleaMarket toEntity(FleaMarketRequest request, Organizer organizer, Address address);
 
     @Mapping(source = "active", target = "isActive")
     FleaMarketResponse fromEntity(FleaMarket fleaMarket);
@@ -27,14 +28,13 @@ public interface FleaMarketMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(source = "description", target = "description")
-    @Mapping(source = "request.address.street", target = "address.street")
-    @Mapping(source = "request.address.zipCity.city", target = "address.zipCity.city")
-    @Mapping(source = "request.address.zipCity.zip", target = "address.zipCity.zip")
-    void updateEntityFromRequest(FleaMarketRequest request, @MappingTarget FleaMarket fleaMarket);
+    @Mapping(source = "address.street", target = "address.street")
+    @Mapping(source = "address.zipCity.city", target = "address.zipCity.city")
+    @Mapping(source = "address.zipCity.zip", target = "address.zipCity.zip")
+    void updateEntityFromRequest(FleaMarketRequest request, Address address, @MappingTarget FleaMarket fleaMarket);
 
-    @Mapping(source = "page.content", target = "fleaMarketResponses", defaultExpression = "java(java.util.Collections.emptyList())")
-    @Mapping(source = "page.size", target = "elementsPerPage")
+    @Mapping(source = "content", target = "fleaMarketResponses", defaultExpression = "java(java.util.Collections.emptyList())")
+    @Mapping(source = "size", target = "elementsPerPage")
     FleaMarketPagedResponse fromPage(Page<FleaMarket> page);
 
 }
