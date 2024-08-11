64ed7adb3721337b02fa59d91b919ab4c7a901ec
Kei-gits <kioo-17@hotmail.com>
2024-08-11 21:36:57 +0200

    fix mapper fleamarketresponse in fleamarkets

--- src/main/java/be/keivy/fleamarketapp/common/dtos/flea_market/responses/FleaMarketPagedResponse.java
+++ src/main/java/be/keivy/fleamarketapp/common/dtos/flea_market/responses/FleaMarketPagedResponse.java
@@ -3,7 +3,7 @@ package be.keivy.fleamarketapp.common.dtos.flea_market.responses;
 import java.util.List;
 
 public record FleaMarketPagedResponse (
-        List<FleaMarketResponse> fleaMarketResponses,
+        List<FleaMarketResponse> fleaMarkets,
         Integer elementsPerPage,
         Long totalElements,
         Integer totalPages
