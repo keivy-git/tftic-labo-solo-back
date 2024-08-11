4b9cc55457614cc387a018b3b2aa92ea09caf76b
Kei-gits <kioo-17@hotmail.com>
2024-08-11 21:37:23 +0200

    add shortdescription, createdat and numberofplaces

--- src/main/java/be/keivy/fleamarketapp/common/dtos/flea_market/responses/FleaMarketResponse.java
+++ src/main/java/be/keivy/fleamarketapp/common/dtos/flea_market/responses/FleaMarketResponse.java
@@ -4,16 +4,20 @@ import be.keivy.fleamarketapp.domain.entities.Address;
 import be.keivy.fleamarketapp.domain.entities.Organizer;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record FleaMarketResponse(
         String title,
         String description,
+        String shortDescription,
         String urlPicture,
         LocalDate dateBegin,
         LocalDate dateEnd,
+        LocalDateTime createdAt,
         Boolean isActive,
         int pricePerMeter,
         int locationPrice,
+        int numberOfPlaces,
         Address address,
         Organizer organizer
         ) { }
