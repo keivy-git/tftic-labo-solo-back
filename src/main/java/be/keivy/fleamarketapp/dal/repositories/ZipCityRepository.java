865e1a2a5e0662c89ae5d71d5f3e4ec948c70fc2
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-08-13 16:15:38 +0200

    add query for fidnByZipAndCity

--- src/main/java/be/keivy/fleamarketapp/dal/repositories/ZipCityRepository.java
+++ src/main/java/be/keivy/fleamarketapp/dal/repositories/ZipCityRepository.java
@@ -2,9 +2,19 @@ package be.keivy.fleamarketapp.dal.repositories;
 
 import be.keivy.fleamarketapp.domain.entities.ZipCity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ZipCityRepository extends JpaRepository<ZipCity, Long> {
+
+    @Query("SELECT zc FROM ZipCity zc WHERE (:zip IS NULL OR zc.zip ilike :zip% ) AND (:city IS NULL OR zc.city ilike :city% )")
+    List<ZipCity> findByZipAndCity(@Param("zip") String zip,
+                                   @Param("city") String city);
+
+
 
 }
